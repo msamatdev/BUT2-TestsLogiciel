0ff1014db9afd5c03563c5a863bb9100e7486bd0
msamatdev <mathis.samat@gmail.com>
2025-05-31 21:23:43 +0200

    Ajout des tests du service web réalisés avec mockMvc, springboot test et JUnit

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Objects;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -27,4 +29,48 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    void testExceptionPasDeVoiture() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenThrow(new ArithmeticException());
+
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testCreerVoiture() throws Exception {
+        String voitureJson = "{\"marque\":\"Nissan\",\"prix\":12000}";
+
+        mockMvc.perform(post("/voiture")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(voitureJson))
+                .andExpect(status().isOk());
+
+        verify(statistiqueImpl, times(1)).ajouter(argThat(voiture ->
+                Objects.equals(voiture.getMarque(), "Nissan") &&
+                voiture.getPrix() == 12000
+        ));
+    }
+
+    @Test
+    void testPrixMoyen() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 11000));
+
+        mockMvc.perform(post("/voiture")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"marque\":\"Nissan\", \"prix\":10000}"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/voiture")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"marque\":\"Mazda\", \"prix\":12000}"))
+                .andExpect(status().isOk());
+
+        // Now call GET /statistique and expect the Echantillon result
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2))
+                .andExpect(jsonPath("$.prixMoyen").value(11000));
+    }
 }
